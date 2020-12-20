cf6befaf26900111facde68a2e13525ce8ea986d
Chris Speciale <christopher.gaf@gmail.com>
2020-12-19 23:33:02 -0500

    Added triangleCulling

--- src/openfl/display/Bitmap3D.hx
+++ src/openfl/display/Bitmap3D.hx
@@ -15,7 +15,7 @@ import openfl.events.Event;
 class Bitmap3D extends Sprite
 {
 	private var __matrix3D:Matrix3D = new Matrix3D();
-	private var __vertices:Vector<Float> = new Vector();
+	private var __vertices:Vector<Float> = new Vector<Float>();
 	private var __uvtData:Vector<Float>=Vector.ofArray([0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0]);
 	private var __indices:Vector<Int> = Vector.ofArray([0, 1, 3, 1, 2, 3]);
 	private var __points:Vector<Float>;
@@ -24,23 +24,25 @@ class Bitmap3D extends Sprite
 	private var __bitmapData:BitmapData;
 	private var __pivotVec:Vector3D = new Vector3D();
 	
-	public var rotationX(default, set):Float;
-	public var rotationY(default, set):Float;
-	public var rotationZ(default, set):Float;
-	public var rotationPX(default, set):Float;
-	public var rotationPY(default, set):Float;
-	public var rotationPZ(default, set):Float;
+	public var rotationX(default, set):Float = 0;
+	public var rotationY(default, set):Float = 0;
+	public var rotationZ(default, set):Float = 0;
+	public var rotationPX(default, set):Float = 0;
+	public var rotationPY(default, set):Float = 0;
+	public var rotationPZ(default, set):Float = 0;
 	
-	public var offsetX(default, set):Float;
-	public var offsetY(default, set):Float;
-	public var offsetZ(default, set):Float;
+	public var offsetX(default, set):Float = 0;
+	public var offsetY(default, set):Float = 0;
+	public var offsetZ(default, set):Float = 0;
 	
-	public var pivotX(default, set):Float; 
-	public var pivotY(default, set):Float; 
-	public var pivotZ(default, set):Float; 
+	public var pivotX(default, set):Float = 0; 
+	public var pivotY(default, set):Float = 0; 
+	public var pivotZ(default, set):Float = 0; 
 	
-	public var fieldOfView:Float;
-	public var focalLength:Float;
+	@:isVar public var fieldOfView(get, set) :Float = 0; 
+	@:isVar public var focalLength(get, set):Float = 0; 
+	
+	public var triangleCulling:TriangleCulling = TriangleCulling.NONE;
 	
 	private var __bitmapHeight:Float;
 	private var __bitmapWidth:Float;
@@ -83,38 +85,28 @@ class Bitmap3D extends Sprite
 	
 	override private function get_y():Float{
 		return __bitmapY;
-	}
-	
-	private function __updateVertices():Void{
-		__vertices = new Vector();
-		//0
-		__vertices.push(this.x);
-		__vertices.push(this.y);
-		__vertices.push(0);
-		//1		
-		__vertices.push(__bitmapWidth);
-		__vertices.push(y);
-		__vertices.push(0);
-		//2
-		__vertices.push(__bitmapWidth);
-		__vertices.push(__bitmapHeight);
-		__vertices.push(0);
-		//3
-		__vertices.push(x);
-		__vertices.push(__bitmapHeight);
-		__vertices.push(0);	
-	}
+	}	
 	
 	public function set_fieldOfView(value:Float):Float
 	{
 		__perspective.fieldOfView = value;
+		__updatePerspective();
 		return value;
 	}
 	
 	public function set_focalLength(value:Float):Float
 	{
 		__perspective.focalLength = value;
+		__updatePerspective();
 		return value;
+	}
+	
+	public function get_fieldOfView():Float{
+		return __perspective.fieldOfView;
+	}
+	
+	public function get_focalLength():Float{
+		return __perspective.focalLength;
 	}
 	
 	public function set_pivotX(x:Float):Float
@@ -210,8 +202,32 @@ class Bitmap3D extends Sprite
 	{
 		removeEventListener(Event.ADDED_TO_STAGE, __onInit);
 
-		fieldOfView = __perspective.fieldOfView = 45;
-		__projectionMatrix = __perspective.toMatrix3D();
+		//fieldOfView = 45;
+		__updatePerspective();
+	}
+	
+	private function __updateVertices():Void{
+		__vertices = new Vector();
+		//0
+		__vertices.push(this.x);
+		__vertices.push(this.y);
+		__vertices.push(0);
+		//1		
+		__vertices.push(__bitmapWidth);
+		__vertices.push(y);
+		__vertices.push(0);
+		//2
+		__vertices.push(__bitmapWidth);
+		__vertices.push(__bitmapHeight);
+		__vertices.push(0);
+		//3
+		__vertices.push(x);
+		__vertices.push(__bitmapHeight);
+		__vertices.push(0);	
+	}
+	
+	private function __updatePerspective():Void{
+	__projectionMatrix = __perspective.toMatrix3D();
 		__updateGraphics();
 	}
 
@@ -231,7 +247,7 @@ class Bitmap3D extends Sprite
 		Utils3D.projectVectors(__matrix3D, __vertices, __points, __uvtData);
 		this.graphics.clear();
 		this.graphics.beginBitmapFill(__bitmapData, null, false, smoothing);
-		this.graphics.drawTriangles(__points, __indices, __uvtData, TriangleCulling.NONE);
+		this.graphics.drawTriangles(__points, __indices, __uvtData, triangleCulling);
 		this.graphics.endFill();
 	}
 
